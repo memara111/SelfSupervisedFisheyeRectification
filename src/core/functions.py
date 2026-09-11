5c65dbf90a012ef75b15ff40370ccacb9faeca67
memara111 <49526147+memara111@users.noreply.github.com>
2026-09-11 22:19:18 +0200

    Update functions.py
    from distortions = [val - random.random() * interval for val in distortions] to distortions = [val + random.random() * interval for val in distortions]

--- src/core/functions.py
+++ src/core/functions.py
@@ -47,6 +47,6 @@ def getDistortions(num_fragments=10, random_values=False):
     distortions = [interval * i for i in range(num_fragments)]
 
     if random_values:
-        distortions = [val - random.random() * interval for val in distortions]
+        distortions = [val + random.random() * interval for val in distortions]
 
     return distortions
