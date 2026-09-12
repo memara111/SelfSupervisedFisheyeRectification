7c904936b85ed5b7583bf8e237e100dca5fa2266
memara111 <49526147+memara111@users.noreply.github.com>
2026-09-12 12:56:00 +0200

    Update functions.py

--- src/core/functions.py
+++ src/core/functions.py
@@ -1,7 +1,14 @@
 import random
 import torch
 
-def train(model=None, dataloader=None, criterion=None, optimizer=None, device='cpu'):
+
+def train(
+    model=None,
+    dataloader=None,
+    criterion=None,
+    optimizer=None,
+    device="cpu",
+):
     running_loss = 0.0
     num_iter = 0
     max_iter = len(dataloader)
@@ -9,23 +16,51 @@ def train(model=None, dataloader=None, criterion=None, optimizer=None, device='c
     model = model.train()
 
     for i, (inputs, labels) in enumerate(dataloader):
-        optimizer.zero_grad()
+        # set_to_none=True avoids an unnecessary fill operation.
+        optimizer.zero_grad(set_to_none=True)
 
-        outputs = model(inputs.to(device))
-        labels = [(x.to(device), y.to(device)) for x, y in labels]
+        # DataLoader uses pinned memory on CUDA, so non_blocking=True can
+        # overlap host->device copies with work more effectively.
+        inputs = inputs.to(
+            device,
+            non_blocking=(device == "cuda"),
+        )
+
+        labels = [
+            (
+                x.to(device, non_blocking=(device == "cuda")),
+                y.to(device, non_blocking=(device == "cuda")),
+            )
+            for x, y in labels
+        ]
+
+        outputs = model(inputs)
         loss = criterion(outputs, labels)
+
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.item()
+        running_loss += loss.detach().item()
         num_iter += 1
 
-        if (i+1) % 100 == 0:
-            print('Iter [{}/{}], Loss = {}'.format(i + 1, max_iter, running_loss / num_iter))
+        if (i + 1) % 100 == 0:
+            print(
+                "Iter [{}/{}], Loss = {}".format(
+                    i + 1,
+                    max_iter,
+                    running_loss / num_iter,
+                )
+            )
 
     return running_loss / num_iter
 
-def val(model=None, dataloader=None, criterion=None, device='cpu'):
+
+def val(
+    model=None,
+    dataloader=None,
+    criterion=None,
+    device="cpu",
+):
     running_loss = 0.0
     num_iter = 0
 
@@ -33,20 +68,36 @@ def val(model=None, dataloader=None, criterion=None, device='cpu'):
 
     with torch.no_grad():
         for inputs, labels in dataloader:
-            outputs = model(inputs.to(device))
-            labels = [(x.to(device), y.to(device)) for x, y in labels]
+            inputs = inputs.to(
+                device,
+                non_blocking=(device == "cuda"),
+            )
+
+            labels = [
+                (
+                    x.to(device, non_blocking=(device == "cuda")),
+                    y.to(device, non_blocking=(device == "cuda")),
+                )
+                for x, y in labels
+            ]
+
+            outputs = model(inputs)
             loss = criterion(outputs, labels)
 
-            running_loss += loss.item()
+            running_loss += loss.detach().item()
             num_iter += 1
 
     return running_loss / num_iter
 
+
 def getDistortions(num_fragments=10, random_values=False):
-    interval = 0.9/(num_fragments-1)
+    interval = 0.9 / (num_fragments - 1)
     distortions = [interval * i for i in range(num_fragments)]
 
     if random_values:
-        distortions = [val - random.random() * interval for val in distortions]
+        distortions = [
+            val - random.random() * interval
+            for val in distortions
+        ]
 
     return distortions
